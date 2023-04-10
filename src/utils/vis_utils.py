18922d6f2ce628cee8a2375051fa1785e2b70626
Faith Johnson <fmjohnson97@aol.com>
2023-04-10 14:53:54 -0400

    testing if this changeds the config path somehow?

--- src/utils/vis_utils.py
+++ src/utils/vis_utils.py
@@ -8,7 +8,7 @@ from habitat.utils.visualizations import maps
 
 AGENT_SPRITE = imageio.imread(
     os.path.join(
-        "/home/faith/GitRepos/habitat-lab/habitat-lab/habitat/utils/visualizations",
+        "~/GitRepos/habitat-lab/habitat-lab/habitat/utils/visualizations",
         "assets",
         "maps_topdown_agent_sprite",
         "100x100.png",
